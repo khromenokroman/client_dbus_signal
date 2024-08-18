39189e6144d4c5ca3cc704f429799d742fe8394a
khromenokroman <roma55592@yandex.ru>
2024-08-18 15:43:49 +0200

    Handle sdbus signal proxy exceptions
    Wrapped signal proxy creation and handling within a try-catch block to capture exceptions and avoid crashes. Modified CMakeLists.txt to correctly link against `sdbus-c++` library.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include <fmt/format.h>
 #include <sdbus-c++/sdbus-c++.h>
 
-static constexpr char const* const DESTINATION = "ru.test.management";
-static constexpr char const* const OBJECT_PATH = "/ru/test/management/converter";
-static constexpr char const* const NAME_SIGNAL = "config_changed";
-static constexpr char const* const INTERFACE_NAME = "ru.test.management.converter.modules";
+static constexpr char const *const DESTINATION = "ru.test.management";
+static constexpr char const *const OBJECT_PATH = "/ru/test/management/converter";
+static constexpr char const *const NAME_SIGNAL = "config_changed";
+static constexpr char const *const INTERFACE_NAME = "ru.test.management.converter.modules";
 
 int main() {
     ::fmt::print("Start prog\n");
+    try {
+        auto converter_signal_proxy = sdbus::createProxy(DESTINATION, OBJECT_PATH);
+        converter_signal_proxy->uponSignal(NAME_SIGNAL).onInterface(INTERFACE_NAME)
+                .call([]() {
+                    ::fmt::print("Catch signal converter\n");
+                });
+        converter_signal_proxy->finishRegistration();
 
-    auto converter_signal_proxy = sdbus::createProxy(DESTINATION,OBJECT_PATH);
-    converter_signal_proxy->uponSignal(NAME_SIGNAL).onInterface(INTERFACE_NAME)
-            .call([]() {
-                ::fmt::print("Catch signal converter\n");
-            });
-
-    sleep(15);
-
+        sleep(15);
+    } catch (std::exception const &ex) {
+        ::fmt::print(stderr, "Exception: {}\n", ex.what());
+        ::fmt::print(stderr, "End prog\n");
+        return -1;
+    }
     ::fmt::print("End prog\n");
-
     return 0;
 }
